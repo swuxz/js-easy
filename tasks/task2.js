00f1aad299c88c767310016e48ed00f0618b50a4
swuxz <sasha20070228@gmail.com>
2025-11-11 11:02:59 +0200

    Update task2.js

--- tasks/task2.js
+++ tasks/task2.js
@@ -2,10 +2,18 @@
 //  при цьому пропускаючи всі цифри.
 
 function reverseWithoutNumbers(str) {
-  // Ваш код тут
+  let filteredChars = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (isNaN(str[i])) {
+      filteredChars.push(str[i]);
+    }
+  }
+
+  return filteredChars.reverse().join('');
 }
 
-console.log(reverseWithoutNumbers("hello123world456")); // Виведе: "dlrowolleh"
-console.log(reverseWithoutNumbers("abc123xyz"));       // Виведе: "zyxabc"
+console.log(reverseWithoutNumbers("hello123world456"));
+console.log(reverseWithoutNumbers("abc123xyz"));
 
 module.exports = reverseWithoutNumbers;

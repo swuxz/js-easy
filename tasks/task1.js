83b99fd1acd4453c4aa3de521f0770b34476e23c
swuxz <sasha20070228@gmail.com>
2025-11-11 11:02:56 +0200

    Update task1.js
    save files

--- tasks/task1.js
+++ tasks/task1.js
@@ -2,7 +2,17 @@
 // на певний символ, наприклад *.
 
 function replaceVowels(str) {
-  // Ваш код тут
+  const vowels = "aeiouy";
+
+  let result = str.split('');
+
+  for (let i = 0; i < result.length; i++) {
+    if (vowels.includes(result[i].toLowerCase())) {
+      result[i] = '*';
+    }
+  }
+
+  return result.join('');
 }
 
 console.log(replaceVowels("hello world")); // Виведе: "h*ll* w*rld"
